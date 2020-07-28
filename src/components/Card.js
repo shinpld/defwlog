37792110818d85e05ae437d8c42041d0995ac74d
shinpld <shinpld@gmail.com>
2020-07-28 16:42:33 +0700

    cleaning

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,24 +8,7 @@ const Card = ({asset_id,desc,desc2,location,bld}) => {
     
     <div>
      
-      {/* <h4 class="f5 fw6 ">{asset_id}</h4>
-        <dl class="f6 lh-title mv2">
-          <dt class="dib b">id:</dt>
-          <dd class="dib ml0 dark-gray">{asset_id}</dd>
-        </dl>
-        <dl class="f6 lh-title mv2">
-          <dt class="dib b">Description:</dt>
-          <dd class="dib ml0 dark-gray">{desc}</dd>
-          <dd class="dib ml0 dark-gray">{desc2}</dd>
-        </dl>
-        <dl class="f6 lh-title mv2">
-          <dt class="dib b">สถานที่ตั้ง:</dt>
-          <dd class="dib ml0 dark-gray">{location}</dd>
-        </dl>
-        <dl class="f6 lh-title mv2">
-          <dt class="dib b">อาคาร / ชั้น:</dt>
-          <dd class="dib ml0 dark-gray">{bld}</dd>
-        </dl> */}
+      
           <div class="f5 fw6 br3 pa3 ma2 bg-light-green  ">
               {asset_id}
           </div>
