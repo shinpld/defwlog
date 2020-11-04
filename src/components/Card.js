68dbe970c2c89cf5abb67f705387e7cc8dfed845
shinpld <shinpld@gmail.com>
2020-11-04 14:03:15 +0700

    First Commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 
@@ -18,29 +17,11 @@ const Card = ({asset_id,desc,desc2,location,bld,sn,room,sup}) => {
               <dt class="dib b">id:</dt>
               <dd class="dib ml0 dark-gray">{asset_id}</dd>
             </div>
-            <div class="dtc-ns tc pv2 bg-black-05">
-              <dt class="dib b">Description:</dt>
-              <dd class="dib ml0 dark-gray">{desc}</dd>
-            </div>
-            <div class="dtc-ns tc pv2 bg-black-10">
-               <dt class="dib b">สถานที่ตั้ง:</dt>
-               <dd class="dib ml0 dark-gray">{location}</dd>
-            </div>
-          </div>
-
+           
           <div className="dt-ns dt--fixed-ns">
             <div class="dtc-ns tc pv2 bg-black-10">
               <dt class="dib b">Serial No: </dt>
               <dd class="dib ml0 dark-gray">{sn}</dd>
-            </div>
-            <div class="dtc-ns tc pv2 bg-black-05">
-              <dt class="dib b">ห้อง: </dt>
-              <dd class="dib ml0 dark-gray">{room}</dd>
-            </div>
-            <div class="dtc-ns tc pv2 bg-black-10">
-               <dt class="dib b">Asset Sup:</dt>
-               <dd class="dib ml0 dark-gray">{sup}</dd>
-            </div>
           </div>
 
        
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,11 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox.js';
 import Scroll from '../components/Scroll.js';
+
 import './App.css';
 import {base,db} from './Base.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import CreateButton from '../components/CreateButton';
 
 class App extends Component{
   constructor(){
@@ -23,42 +20,62 @@ class App extends Component{
   onSearchChange = (event) => {
     this.setState({searchfield: event.target.value})
   }
+  generateLog(){ 
+    // Changing state 
+    
+    var immediatelyAvailableReference = base.push('messages', {
+      data: {message: 'pushed',reason: 'reason1', },
+      then(err){
+        if(!err){
+          
+        }
+      }
+    });
+    //available immediately, you don't have to wait for the callback to be called
+    var generatedKey = immediatelyAvailableReference.key;
+  } 
+
+  onSubmit(){
+
+  }
+  
+  
 
   componentDidMount(){
-    /* fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users=>{
-        this.setState({robots:users})
-      }); */
+   
       
    
-        base.syncState(`AeroAsset`, {
+        base.syncState(`messages`, {
           context: this,
           state: 'items',
           asArray: true
         });
-      
   }
 
 
   render(){
     const {items, searchfield} =this.state;
-    const filteredItems = this.state.items
-    .filter(items=>{
-      return (
-    items.Asset_Sup.toString().toLowerCase().includes(searchfield.toLowerCase())
-    );
+    const filteredItems = this.state.items.filter(items=>{
+      return (items.message.toString().toLowerCase().includes(searchfield.toLowerCase()))
+    });
 
-    }  )
 
-    
+   
     if(!items.length){
       return <h1>Loading...</h1>
     }
 
     return (
       <div className='tc'>
-        <h1 className='f1'> AssetViewer </h1>
+        
+        <Button variant="contained" color="secondary" onClick={this.generateLog} >
+            Hello World
+        </Button>
+
+        <CreateButton addLog={this.generateLog} />
+          
+        <h1 className='f1'> FWLog </h1>
+        
         <SearchBox searchChange={this.onSearchChange}/>
         <Scroll>
           <CardList items={filteredItems.splice(0,30)} />
